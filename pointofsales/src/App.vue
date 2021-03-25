cb70e0081cbbad650413307ce8f369498574b83c
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-25 23:02:42 +0700

    Login value style

--- pointofsales/src/App.vue
+++ pointofsales/src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <Navbar />
-    <Layout />
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <Navbar />
+    <Layout /> -->
+    <Login />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
-import Navbar from "./components/Navbar.vue";
-import Layout from "./components/Layout.vue";
+// import Navbar from "./components/Navbar.vue";
+// import Layout from "./components/Layout.vue";
+import Login from "./pages/Login.vue";
 
 export default {
   name: "App",
   components: {
-    Navbar,
-    Layout,
+    Login,
+    // Navbar,
+    // Layout,
   },
 };
 </script>
@@ -27,6 +27,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /* color: #2c3e50; */
+  background-color: #2D3748;
 }
 </style>
