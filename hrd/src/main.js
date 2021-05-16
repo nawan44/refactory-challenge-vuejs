f209bcbfe9fc41ca34510b0ea2a538ce32434483
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-05-16 23:39:15 +0700

    clear bug

--- hrd/src/main.js
+++ hrd/src/main.js
@@ -1,19 +1,13 @@
-import Vue from 'vue'
+// import Vue from "vue";
 
-// import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { BootstrapVue } from 'bootstrap-vue'
-import './plugins/bootstrap-vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
+// import { BootstrapVue } from "bootstrap-vue";
+// import "./plugins/bootstrap-vue";
 
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
-Vue.config.productionTip = false
-
-// createApp(App).mount('#app')
-new Vue({
-    render: h => h(App)
-}).$mount("#app");
+createApp(App).mount("#app");
+// new Vue({
+//   render: (h) => h(App),
+// }).$mount("#app");
